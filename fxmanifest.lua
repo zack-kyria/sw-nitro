f1faf33813da5638517f830d9919b82541f0ad81
zack-kyria <56772875+zack-kyria@users.noreply.github.com>
2021-12-29 23:25:56 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'sw-nitro'
+description 'The most advanced nitro system for FiveM ~ Modified By ZAXOS'
+author 'Deltanic - https://github.com/Deltanic/'
+url 'https://github.com/zack-kyria/sw-nitro/'
+
+client_script 'client/utils.lua'
+client_script 'client/fuel.lua'
+client_script 'client/ptfx.lua'
+client_script 'client/boost.lua'
+client_script 'client/purge.lua'
+client_script 'client/trails.lua'
+client_script 'client/main.lua'
+
+server_script '@es_extended/locale.lua'
+server_script 'server/main.lua'
+
+dependency 'es_extended'
+
